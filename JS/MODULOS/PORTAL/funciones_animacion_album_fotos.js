6c41d0cff5235f8253f4f8342bb72cc4726532d0
akabane89 <akabane89@gmail.com>
2012-03-17 06:56:27 +0000

    Album de fotos terminado

--- JS/MODULOS/PORTAL/funciones_animacion_album_fotos.js
+++ JS/MODULOS/PORTAL/funciones_animacion_album_fotos.js
@@ -40,4 +40,77 @@ $("#galeria_fotos #salir_album_fotos").click(function(){
 
 
 
+$("#marco_foto_mostrada #boton_cerrar").click(function(){
+	
+	$("#marco_foto_mostrada").fadeOut(GLOBAL_VEL_FADE);	
+	$("#protector_marco_foto").fadeOut(GLOBAL_VEL_FADE);	
+	
 });
+
+
+
+});
+
+
+function click_minifoto_muestra(background){
+
+	var margen_foto_marco=20;
+
+	var values=background.split("IMAGENES");
+
+
+	$("#marco_foto_mostrada").fadeIn(GLOBAL_VEL_FADE);
+	
+	
+	$("#marco_foto_mostrada img").attr("src","../IMAGENES"+values[1].substring(0,values[1].length-1));
+	
+	$("#marco_foto_mostrada img").removeAttr("width");
+	$("#marco_foto_mostrada img").removeAttr("height");
+	
+	var width_real_imagen=$("#marco_foto_mostrada img").width();
+	var height_real_imagen=$("#marco_foto_mostrada img").height();
+	
+	//////////////////////////////Le damos al marco el valor que le corresponde en su ancho y alto
+	
+	var ancho_pantalla=$("body").css("width");
+	var alto_pantalla=$("body").css("height");
+	
+
+	
+	if(parseFloat(ancho_pantalla)<=(parseFloat(width_real_imagen)+margen_foto_marco*2)){
+	/*	alert("mas ancho");
+		alert(ancho_pantalla);*/
+		width_real_imagen=parseFloat(ancho_pantalla)-60;
+		$("#marco_foto_mostrada img").attr("width",width_real_imagen+"px");
+		
+		height_real_imagen=$("#marco_foto_mostrada img").height();
+	}
+	
+	if(parseFloat(alto_pantalla)<=(parseFloat(height_real_imagen)+margen_foto_marco*2)){
+	/*	alert("mas alto");
+		alert(alto_pantalla);*/
+		height_real_imagen=parseFloat(alto_pantalla)-60;
+		
+		$("#marco_foto_mostrada img").attr("height",height_real_imagen+"px");
+		
+		width_real_imagen=$("#marco_foto_mostrada img").width();
+	}	
+	
+	
+	$("#marco_foto_mostrada ").css("width",(parseFloat(width_real_imagen)+margen_foto_marco*2)+"px");
+	$("#marco_foto_mostrada ").css("height",(parseFloat(height_real_imagen)+margen_foto_marco*2)+"px");
+	
+	
+	var margen_left=(parseFloat(ancho_pantalla)-(parseFloat(width_real_imagen)+margen_foto_marco*2))/2;
+	var margen_top=(parseFloat(alto_pantalla)-(parseFloat(height_real_imagen)+margen_foto_marco*2))/2;
+		
+	
+	$("#marco_foto_mostrada ").css("margin-left",margen_left+"px");
+	$("#marco_foto_mostrada ").css("margin-top",margen_top+"px");
+		
+	$("#marco_foto_mostrada").fadeIn(GLOBAL_VEL_FADE);	
+	$("#protector_marco_foto").fadeIn(GLOBAL_VEL_FADE);	
+	
+
+	
+}
