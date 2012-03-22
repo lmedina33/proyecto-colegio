bfc4612865e1116905609c4ce130acad85810243
danielViguria29@gmail.com <danielViguria29@gmail.com@2e052854-11cd-88a1-dd37-a88cb04bb6d4>
2012-03-22 18:04:37 +0000

    se corrigio la funcion "cambiar_profesor"
    ya q se me habia olvidado considerar un parametro

--- trunk/POST/ADMINISTRACION/GRADO/Cambiar_profesor.php
+++ trunk/POST/ADMINISTRACION/GRADO/Cambiar_profesor.php
@@ -4,7 +4,7 @@ require_once("../../../DAO/ADMINISTRACION/DAOCursos.php");
 
 $gestion_grado=new DAOCursos();
 
-$respuesta=$gestion_grado->cambiar_profesor("3","P","lenguaje","nuevo_ID_profesor");
+$respuesta=$gestion_grado->cambiar_profesor("3","P","arte","A","nuevo_id_mejorado");
 
 
 echo $respuesta;
