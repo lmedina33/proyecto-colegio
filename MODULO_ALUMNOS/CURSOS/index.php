933cea581abdf7553b24e1d98f32080c36b0aecb
akabane89 <akabane89@gmail.com>
2012-03-24 00:20:57 +0000

    Interfaz de módulo alumno avanzada

---
+++ MODULO_ALUMNOS/CURSOS/index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
+	<meta name="author" content="Dante Vidal Tueros, Analista Desarrollador"/>
+
+
+	<script language="javascript" src="../../JS/LIBRERIAS/jquery1_6.js"></script>
+	<script language="javascript" src="../../JS/FUNCIONES/funciones_interfaz_grafica.js"></script>
+	<script language="javascript" src="../../JS/FUNCIONES/funciones_variables_globales.js"></script>
+
+	<script language="javascript" src="../../JS/MODULOS/MODULO_ALUMNOS/CURSOS/funciones_click.js"></script>
+	<script language="javascript" src="../../JS/MODULOS/MODULO_ALUMNOS/CURSOS/funciones_animacion_menu_acordeon.js"></script>
+	<script language="javascript" src="../../JS/MODULOS/MODULO_ALUMNOS/funciones_post.js"></script>
+	<script language="javascript" src="../../JS/MODULOS/MODULO_ALUMNOS/funciones_carga_pagina.js"></script>
+	
+	
+	<link rel="stylesheet" type="text/css" href="../../CSS/general_tabla.css" media="screen"/> 
+	<link rel="stylesheet" type="text/css" href="../../CSS/estilo_base.css" media="screen"/>  
+	<link rel="stylesheet" type="text/css" href="../../CSS/MODULO_ALUMNOS/CURSOS/general.css" media="screen"> 
+	<link rel="stylesheet" type="text/css" href="../../CSS/MODULO_ALUMNOS/CURSOS/transiciones_efectos.css" media="screen"> 
+	<title>Untitled 3</title>
+</head>
+
+<body>
+
+<div id="protector_transparente"></div>
+<div id="div_back"></div>
+<div id="div_back2"></div>
+<div id="div_back3"></div>
+
+
+
+<div id="cabecera-pagina">
+	
+	<div id="logotipo">
+	
+	</div>
+</div>
+
+
+
+<div id="cuerpo-pagina">
+
+
+	<div id="menu_vertical">
+		<div id="titulo_menu">Mis Cursos</div>
+			
+			<div id="opcion-curso_1" title="Seleccionado"class="opcion"><div id="curso-matematica"  class="texto_pestana">Matemática</div></div>
+				
+				
+			<div id="opcion-curso_2"  class="opcion"><div id="curso-matematica" class="texto_pestana">Matemática</div></div>			
+			<div id="opcion-curso_3"  class="opcion"><div id="curso-matematica" class="texto_pestana">Matemática</div></div>			
+			<div id="opcion-curso_4"  class="opcion"><div id="curso-matematica" class="texto_pestana">Matemática</div></div>			
+			<div id="opcion-curso_5"  class="opcion"><div id="curso-matematica" class="texto_pestana">Matemática</div></div>			
+			<div id="opcion-curso_6"  class="opcion"><div id="curso-matematica" class="texto_pestana">Matemática</div></div>			
+			<div id="opcion-curso_7"  class="opcion"><div id="curso-matematica" class="texto_pestana">Matemática</div></div>			
+			<div id="opcion-curso_8"  class="opcion"><div id="curso-matematica" class="texto_pestana">Matemática</div></div>			
+			<div id="opcion-curso_9"  class="opcion"><div id="curso-matematica" class="texto_pestana">Matemática</div></div>			
+			<div id="opcion-curso_10"  class="opcion"><div id="curso-matematica" class="texto_pestana">Matemática</div></div>			
+			<div id="opcion-curso_10"  class="opcion"><div id="curso-matematica" class="texto_pestana">Matemática</div></div>			
+			<div id="opcion-curso_10"  class="opcion"><div id="curso-matematica" class="texto_pestana">Matemática</div></div>			
+			<div id="opcion-curso_10"  class="opcion"><div id="curso-matematica" class="texto_pestana">Matemática</div></div>			
+			<div id="opcion-curso_10"  class="opcion"><div id="curso-matematica" class="texto_pestana">Matemática</div></div>			
+			<div id="opcion-curso_10"  class="opcion"><div id="curso-matematica" class="texto_pestana">Matemática</div></div>			
+			<div id="opcion-curso_10"  class="opcion"><div id="curso-matematica" class="texto_pestana">Matemática</div></div>			
+			<div id="opcion-curso_10"  class="opcion"><div id="curso-matematica" class="texto_pestana">Matemática</div></div>			
+			<div id="opcion-curso_10"  class="opcion"><div id="curso-matematica" class="texto_pestana">Matemática</div></div>			
+			<div id="opcion-curso_4"  class="opcion"><div id="curso-matematica" class="texto_pestana">Matemática</div></div>			
+			<div id="opcion-curso_4"  class="opcion"><div id="curso-matematica" class="texto_pestana">Matemática</div></div>			
+					
+	</div>
+		
+			<div id="contenido-opciones">
+				<div class="opcion" id="opcion-notas" title="Seleccionado" ><div  class="texto_pestana">Notas</div></div>
+				<div class="opcion" id="opcion-observaciones" ><div  class="texto_pestana">Observaciones</div></div>
+			</div>
+		<div id="contenido-pagina">
+			
+		</div>
+</div>
+		
+
+
+
+
+</body>
+</html>
