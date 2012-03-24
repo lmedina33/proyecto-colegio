207eddb9a9c596fb666650b5f8418505539dcafc
danielViguria29 <danielViguria29@gmail.com>
2012-03-24 19:43:58 +0000

    Aumentar un campo mas a la tabla admin_cursos y modificar los DAO necesarios
    q pendeibis era la modificacion
    
    xD
    
    me iso demorar y pensar un poco mas xD

--- POST/ADMINISTRACION/GRADO/Obtener_grado_curso.php
+++ POST/ADMINISTRACION/GRADO/Obtener_grado_curso.php
@@ -4,7 +4,7 @@ require_once("../../../DAO/ADMINISTRACION/DAOCursos.php");
 
 $gestion_grado=new DAOCursos();
 
-$respuesta=$gestion_grado->obtener_cursos_grado("3","P");
+$respuesta=$gestion_grado->obtener_cursos_grado("4","P");
 
 
 echo $respuesta;
